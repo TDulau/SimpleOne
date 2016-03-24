6903d0c1c8b87a7b5d2e764ecf31223956fbb46f
Tudor <tudor.calin.dulau@gmail.com>
2016-03-24 12:38:02 +0200

    - modify previous commit

---
+++ src/com/example/hello/Simple.java
@@ -0,0 +1,18 @@
+package com.example.hello;
+
+/**
+ * Created by Tudor on 3/22/2016.
+ */
+public class Simple {
+    public static void main(String[] args) {
+
+        int div = 5;
+
+        for (int i = 0; i < 14; i++) {
+            System.out.println(i + ": I'm saying HI!");
+            if (i % div == 0) {
+                System.out.println("    " + i + " -> am a multiple of " + div);
+            }
+        }
+    }
+}
